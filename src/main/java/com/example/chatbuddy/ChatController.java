567f602df53561190a6523d23f3367e97b4afa24
Favas K P <183742743+favas-kp@users.noreply.github.com>
2025-08-28 12:38:18 +0530

    Adds notification send end point (send?token=abc)

--- src/main/java/com/example/chatbuddy/ChatController.java
+++ src/main/java/com/example/chatbuddy/ChatController.java
@@ -1,5 +1,8 @@
 package com.example.chatbuddy;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,4 +17,27 @@ public class ChatController {
     public String getGreet(@RequestParam String name) {
         return "Hello " + name + "!";
     }
+
+    @GetMapping("/send")
+    public String getSend(@RequestParam String token) {
+        final Message msg = Message.builder()
+            .setToken(token)
+            .setNotification(
+                Notification.builder()
+                    .setTitle("Spring Boot Push")
+                    .setBody("This is for testing ...")
+                    .build()
+            )
+            .build();
+        try {
+            String response = FirebaseMessaging.getInstance().send(msg);
+            return "Message sent: " + response;
+        } catch (Exception e) {
+            return "Error: " + e.getMessage();
+        }
+    }
 }
+
+// --- Tokens ---
+// samsung: cif1IhjITSu5_k0T8Svewp:APA91bHgyOL0H4oGTEyiTNfZvdIMHKeYAQuzire9-ELXDXjYglIzpLFfVL3oG7ikjrRpuY2da6TB_Jm-JSR3RJ9D63P5JheaeR1VY9-HH29Hl1NsOGAjFuw
+// Emulator: eAL8sG-cQoKf143HNPdvRn:APA91bGKOF6b5geyxWjWyeolZPzw_Sw_U6spI38XX2qBcVTA4bhOTeCYKnziaQGo-roCLHFk1MI-n4yUKmrR_n84kjYzD_O36dx4ppPnMhA2WHjpU7QsfGQ
---
+++ src/main/java/com/example/chatbuddy/config/FirebaseConfig.java
@@ -0,0 +1,27 @@
+package com.example.chatbuddy.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void init() throws IOException {
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(
+                            new ClassPathResource("chat-buddy-263-firebase.json").getInputStream()
+                    ))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        }
+    }
+}
